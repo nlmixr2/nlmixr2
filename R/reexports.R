2ce9652f28aafd5aca2967e35b2d9bb8acb79c50
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-20 10:49:28 -0600

    First commit

---
+++ R/reexports.R
@@ -0,0 +1,231 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom rxode2 rxode2
+#' @export
+rxode2::rxode2
+
+#' @importFrom rxode2 rxode
+#' @export
+rxode2::rxode
+
+#' @importFrom rxode2 RxODE
+#' @export
+rxode2::RxODE
+
+#' @importFrom rxode2 ini
+#' @export
+rxode2::ini
+
+#' @importFrom rxode2 model
+#' @export
+rxode2::model
+
+#' @importFrom rxode2 lotri
+#' @export
+rxode2::lotri
+
+
+#' @importFrom rxode2 logit
+#' @export
+rxode2::logit
+
+#' @importFrom rxode2 expit
+#' @export
+rxode2::expit
+
+#' @importFrom rxode2 probit
+#' @export
+rxode2::probit
+
+#' @importFrom rxode2 probitInv
+#' @export
+rxode2::probitInv
+
+#' @importFrom rxode2 rxSolve
+#' @export
+rxode2::rxSolve
+
+#' @importFrom rxode2 rxClean
+#' @export
+rxode2::rxClean
+
+#' @importFrom rxode2 rxCat
+#' @export
+rxode2::rxCat
+
+#' @importFrom nlmixr2 nlme
+#' @export
+nlmixr2est::nlme
+
+#' @importFrom nlmixr2 ACF
+#' @export
+nlmixr2est::ACF
+
+#' @importFrom nlmixr2 VarCorr
+#' @export
+nlmixr2est::VarCorr
+
+#' @importFrom nlmixr2 getVarCov
+#' @export
+nlmixr2est::getVarCov
+
+
+#' @importFrom nlmixr2 augPred
+#' @export
+nlmixr2est::augPred
+
+#' @importFrom nlmixr2 fixef
+#' @export
+nlmixr2est::fixef
+
+#' @importFrom nlmixr2 fixed.effects
+#' @export
+nlmixr2est::fixed.effects
+
+#' @importFrom nlmixr2 ranef
+#' @export
+nlmixr2est::ranef
+
+#' @importFrom nlmixr2 random.effects
+#' @export
+nlmixr2est::random.effects
+
+#' @importFrom rxode2 eventTable
+#' @export
+rxode2::eventTable
+
+#' @importFrom rxode2 add.dosing
+#' @export
+rxode2::add.dosing
+
+#' @importFrom rxode2 add.sampling
+#' @export
+rxode2::add.sampling
+
+#' @importFrom nlmixr2 pdDiag
+#' @export
+nlmixr2est::pdDiag
+
+#' @importFrom nlmixr2 pdSymm
+#' @export
+nlmixr2est::pdSymm
+
+#' @importFrom nlmixr2 pdLogChol
+#' @export
+nlmixr2est::pdLogChol
+
+#' @importFrom nlmixr2 pdIdent
+#' @export
+nlmixr2est::pdIdent
+
+#' @importFrom nlmixr2 pdCompSymm
+#' @export
+nlmixr2est::pdCompSymm
+
+#' @importFrom nlmixr2 pdBlocked
+#' @export
+nlmixr2est::pdBlocked
+
+#' @importFrom nlmixr2 pdNatural
+#' @export
+nlmixr2est::pdNatural
+
+#' @importFrom nlmixr2 pdConstruct
+#' @export
+nlmixr2est::pdConstruct
+
+#' @importFrom nlmixr2 pdFactor
+#' @export
+nlmixr2est::pdFactor
+
+#' @importFrom nlmixr2 pdMat
+#' @export
+nlmixr2est::pdMat
+
+#' @importFrom nlmixr2 pdMatrix
+#' @export
+nlmixr2est::pdMatrix
+
+#' @importFrom nlmixr2 reStruct
+#' @export
+nlmixr2est::reStruct
+
+#' @importFrom nlmixr2 varWeights
+#' @export
+nlmixr2est::varWeights
+
+#' @importFrom nlmixr2 varPower
+#' @export
+nlmixr2est::varPower
+
+#' @importFrom nlmixr2 varFixed
+#' @export
+nlmixr2est::varFixed
+
+#' @importFrom nlmixr2 varFunc
+#' @export
+nlmixr2est::varFunc
+
+#' @importFrom nlmixr2 varExp
+#' @export
+nlmixr2est::varExp
+
+#' @importFrom nlmixr2 varConstPower
+#' @export
+nlmixr2est::varConstPower
+
+#' @importFrom nlmixr2 varIdent
+#' @export
+nlmixr2est::varIdent
+
+#' @importFrom nlmixr2 varComb
+#' @export
+nlmixr2est::varComb
+
+#' @importFrom nlmixr2 groupedData
+#' @export
+nlmixr2est::groupedData
+
+#' @importFrom nlmixr2 getData
+#' @export
+nlmixr2est::getData
+
+#' @importFrom rxode2 et
+#' @export
+rxode2::et
+
+#' @importFrom rxode2 rxParams
+#' @export
+rxode2::rxParams
+
+#' @importFrom rxode2 rxParam
+#' @export
+rxode2::rxParam
+
+
+#' @importFrom rxode2 et
+#' @export
+rxode2::et
+
+#' @importFrom rxode2 geom_cens
+#' @export
+rxode2::geom_cens
+
+#' @importFrom rxode2 geom_amt
+#' @export
+rxode2::geom_amt
+
+
+#' @importFrom rxode2 stat_cens
+#' @export
+rxode2::stat_cens
+
+#' @importFrom rxode2 stat_amt
+#' @export
+rxode2::stat_amt
+
+#' @importFrom rxode2 rxControl
+#' @export
+rxode2::rxControl
---
+++ README.md
@@ -0,0 +1,28 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# nlmixr2
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of nlmixr2 is to support easy and robust nonlinear mixed
+effects models in R
+
+## Installation
+
+You can install the development version of nlmixr2 like so:
+
+``` r
+remotes::install_github("nlmixr2/nlmixr2")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(nlmixr2)
+## basic example code
+```
