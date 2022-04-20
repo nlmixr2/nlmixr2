29767078300f0d3d0ae40d60cfd3f05f78ab4222
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-04-20 13:28:23 -0500

    Update for check

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,6 @@
+#' @import nlmixr2data
 #' @import nlmixr2plot
-#'
+#' @importFrom stats predict logLik na.fail pchisq approxfun cov cov2cor dlnorm median na.omit qchisq qnorm
 .genHardReExport <- function(fun) {
   .args <- deparse(eval(str2lang(paste0("args(", fun, ")"))))
   .args <- .args[-length(.args)]
