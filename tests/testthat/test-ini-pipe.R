b58da0944cb026ba0ecbef94a0cd9054a4dc62d9
matt <matthew.fidler@gmail.com>
2022-09-16 16:39:25 -0500

    Add ini piping

---
+++ tests/testthat/test-ini-pipe.R
@@ -0,0 +1,23 @@
+test_that("ini piping", {
+  model <- function () {
+    description <- "One compartment PK model with linear clearance"
+    ini({
+      lka <- 0.45
+      label("Absorption rate (Ka)")
+      lcl <- 1
+      label("Clearance (CL)")
+      lvc <- 3.45
+      label("Central volume of distribution (V)")
+      prop.err <- c(0, 0.5)
+      label("Proportional residual error (fraction)")
+    })
+    model({
+      ka <- exp(lka + etalka)
+      cl <- exp(lcl)
+      vc <- exp(lvc)
+      linCmt() ~ prop(prop.err)
+    })
+  }
+
+  expect_error(nlmixr2::ini(model, etalka~1), NA)
+})
